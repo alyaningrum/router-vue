069eac1ba5b6b9b1fbc551ce26f5742590544c94
alyaningrum <alyaningrum17@gmail.com>
2022-03-27 15:17:35 +0700

    router-vue

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+<nav class="navbar navbar-expand-lg navbar-dark bg-success">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">GPP System</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link active" aria-current="page" to="/">dashboard</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">about</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/user">user</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<router-view />
+</template>
---
+++ src/pages/user/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Halaman User</h1> 
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style>
+ h1 {
+        font-size: 25px;
+        padding-top: 50px;
+        text-align: center;
+    }
+</style>
